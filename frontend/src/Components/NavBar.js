9789ad5212a20e720753b4d08f457a16260ac169
PulinaHewawasam <pulinahewawasam@gmail.com>
2026-04-09 12:02:47 +0530

    Add Navbar and HomePage components; update package.json scripts

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__brand">Smart Campus</div>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <main className="home-page">
+      <h1>Welcome to Smart Campus</h1>
+      <p>The frontend is now loading correctly.</p>
+    </main>
+  );
+}
+
+export default HomePage;
